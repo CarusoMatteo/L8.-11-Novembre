8fd4c795971cfb980a05885162283b8e83409530
Matteo Caruso <matteo.caruso7@studio.unibo.it>
2024-11-11 12:41:20 +0100

    Completed 82 by implementing DeathNoteImplementation

--- 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -1,49 +1,116 @@
 package it.unibo.deathnote.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import it.unibo.deathnote.api.DeathNote;
 
 public class DeathNoteImplementation implements DeathNote {
 
+    private static final String DEFAULT_DEATH_CAUSE = "heart attack";
+
+    private final Map<String, String> deathCauses = new HashMap<>();
+    private final Map<String, String> details = new HashMap<>();
+
+    private String latestNameWritten;
+    private long latestWriteTime;
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public String getRule(int ruleNumber) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRule'");
+    public String getRule(final int ruleNumber) {
+        try {
+            return RULES.get(ruleNumber - 1);
+        } catch (final Exception e) {
+            throw new IllegalArgumentException("Rule " + ruleNumber + "is not a valid rule.");
+        }
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void writeName(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeName'");
+    public void writeName(final String name) {
+        Objects.requireNonNull(name);
+
+        this.latestNameWritten = name;
+
+        this.deathCauses.put(this.latestNameWritten, null);
+        this.details.put(this.latestNameWritten, null);
+        this.latestWriteTime = System.currentTimeMillis();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public boolean writeDeathCause(String cause) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDeathCause'");
+    public boolean writeDeathCause(final String cause) {
+        if (this.latestNameWritten == null) {
+            throw new IllegalStateException("No name was written while attempting to write death cause.");
+        }
+        if (cause == null) {
+            throw new IllegalStateException("cause is null.");
+        }
+
+        if (getElapsedTime() <= 40) {
+            this.deathCauses.put(this.latestNameWritten, cause);
+            return true;
+        }
+
+        return false;
     }
 
-    @Override
-    public boolean writeDetails(String details) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'writeDetails'");
+    private long getElapsedTime() {
+        return System.currentTimeMillis() - this.latestWriteTime;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public String getDeathCause(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathCause'");
+    public boolean writeDetails(final String details) {
+        if (this.latestNameWritten == null) {
+            throw new IllegalStateException("No name was written while attempting to write details: " + details);
+        }
+        if (details == null) {
+            throw new IllegalStateException("details is null.");
+        }
+
+        if (getElapsedTime() <= 6040) {
+            this.details.put(latestNameWritten, details);
+            return true;
+        }
+
+        return false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public String getDeathDetails(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDeathDetails'");
+    public String getDeathCause(final String name) {
+        final String deathCause = this.deathCauses.get(name);
+
+        return deathCause != null ? deathCause : DEFAULT_DEATH_CAUSE;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public boolean isNameWritten(String name) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isNameWritten'");
+    public String getDeathDetails(final String name) {
+        final String details = this.details.get(name);
+
+        return details != null ? details : "";
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isNameWritten(final String name) {
+        return this.deathCauses.containsKey(name);
+    }
 }
